ca5605df81e1d78a7ae440b7a37ff2ce4e2ef4bd
Viktor <Benedictum1@mail.ru>
2017-08-14 18:38:26 +0300

    first commit

--- src/main/java/com/viktor/yurlov/messages/Message.java
+++ src/main/java/com/viktor/yurlov/messages/Message.java
@@ -7,7 +7,7 @@ import java.util.ResourceBundle;
  * Created by Viktor on 14.08.17.
  */
 public abstract class Message {
-    private static final String MESSAGES_File = "messages";
+    private static final String MESSAGES = "messages";
     private static final String WORLD = "world";
     private static final String END = "end";
 
@@ -15,10 +15,10 @@ public abstract class Message {
 
     public String getMessage() {
         StringBuilder ms = new StringBuilder();
-        ms.append(ResourceBundle.getBundle(MESSAGES_File, Locale.getDefault()).getString(getTimeOfDay()))
+        ms.append(ResourceBundle.getBundle(MESSAGES, Locale.getDefault()).getString(getTimeOfDay()))
                 .append(", ")
-                .append(ResourceBundle.getBundle(MESSAGES_File, Locale.getDefault()).getString(WORLD))
-                .append(ResourceBundle.getBundle(MESSAGES_File, Locale.getDefault()).getString(END));
+                .append(ResourceBundle.getBundle(MESSAGES, Locale.getDefault()).getString(WORLD))
+                .append(ResourceBundle.getBundle(MESSAGES, Locale.getDefault()).getString(END));
         return ms.toString();
     }
     public abstract String getTimeOfDay();
